f821cdd7e5b1bfa90ba4ce5937a85ede071d0129
Diya Mariya Davis <diya220473@gmail.com>
2026-09-26 17:01:49 +0530

    Added 3 Scanners in 06_particle_scanner_sketch.js

--- 06_particle_scanner_sketch.js
+++ 06_particle_scanner_sketch.js
@@ -7,38 +7,55 @@ const blueRange1Size = 50;
 const blueRange2X = 200;
 const blueRange2Y = 0;
 const blueRange2Size = 5;
+const blueRange3X = 0;
+const blueRange3Y = 50;
+const blueRange3Size = 20;
 let scannerX = 0;
 let scannerY = 0;
 let scannerSize = 20;
+let scannerX1 = windowWidth - scannerSize;
+let scannerX2 = 0;
+let scannerY2 = 0;
 let speed = 2;
 let speed1 = -1;
-let scannerX1 = windowWidth - scannerSize;
+let speed2 = 1;
 
 function setup() {
     r.InitWindow(windowWidth, windowHeight, "Particle Scanner");
     r.SetTargetFPS(60);
 }
 
-function getScannerColor(scannerXCoord) {
+function getScannerColorX(scannerXCoord) {
     const touchingRange1 = scannerXCoord + scannerSize >= blueRange1X && scannerXCoord <= blueRange1X + blueRange1Size;
     const touchingRange2 = scannerXCoord + scannerSize >= blueRange2X && scannerXCoord <= blueRange2X + blueRange2Size;
+
     if (touchingRange1 || touchingRange2) {
         return r.RED;
     }
     return r.WHITE;
 }
-
+function getScannerColorY(scannerYCoord) {
+    const touchingRange3 = scannerYCoord + scannerSize >= blueRange3Y && scannerYCoord <= blueRange3Y + blueRange3Size;
+    if (touchingRange3) {
+        return r.RED;
+    }
+    return r.WHITE;
+}
 function draw() {
     let scannerColor;
     let scannerColor1;
+    let scannerColor2;
     r.BeginDrawing();
     r.ClearBackground(r.BLACK);
     r.DrawRectangle(blueRange1X, blueRange1Y, blueRange1Size, windowHeight, r.BLUE);
-    r.DrawRectangle(blueRange2X, blueRange2Y, blueRange2Size, windowHeight, r.BLUE)
-    scannerColor = getScannerColor(scannerX);
-    scannerColor1 = getScannerColor(scannerX1);
+    r.DrawRectangle(blueRange2X, blueRange2Y, blueRange2Size, windowHeight, r.BLUE);
+    r.DrawRectangle(blueRange3X, blueRange3Y, windowWidth, blueRange3Size, r.BLUE);
+    scannerColor = getScannerColorX(scannerX);
+    scannerColor1 = getScannerColorX(scannerX1);
+    scannerColor2 = getScannerColorY(scannerY2);
     r.DrawRectangle(scannerX, scannerY, scannerSize, windowHeight, scannerColor);
     r.DrawRectangle(scannerX1, scannerY, scannerSize, windowHeight, scannerColor1);
+    r.DrawRectangle(scannerX2, scannerY2, windowWidth, scannerSize, scannerColor2);
     r.EndDrawing();
 }
 
@@ -51,8 +68,13 @@ function update() {
     if (scannerX1 <= windowWidth / 2 || scannerX1 + scannerSize > windowWidth) {
         speed1 = -speed1;
     }
+    if (scannerY2 + scannerSize >= windowHeight || scannerY2 < 0) {
+        speed2 = -speed2;
+
+    }
     scannerX = scannerX + speed;
     scannerX1 = scannerX1 + speed1;
+    scannerY2 = scannerY2 + speed2;
 
 }
 
